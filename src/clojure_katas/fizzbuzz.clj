fb03149b46371f843f16dbbe61031546032bee0d
Devin Walters <devinw@gmail.com>
2011-05-08 16:14:27 -0500

    fizzbuzz

--- src/clojure_katas/fizzbuzz.clj
+++ src/clojure_katas/fizzbuzz.clj
@@ -5,5 +5,48 @@
 ;; and "fizzbuzz" for n divisible by 5 and 3
 ;; otherwise just the number
 
-(defn answer [n])
+(defn fizz
+  "Modulo n and 3"
+  [n]
+  (mod n 3))
 
+(defn buzz
+  "Modulo n and 5"
+  [n]
+  (mod n 5))
+
+(defn fizz?
+  "Is n divisible by 3?"
+  [n]
+  (zero? (fizz n)))
+
+(defn buzz?
+  "Is n divisible by 5?"
+  [n]
+  (zero? (buzz n)))
+
+(defn fizzbuzz?
+  "Is n divisible by both 5 and 3?"
+  [n]
+  (zero? (+ (fizz n) (buzz n))))
+
+(defn fizzbuzz
+  "Returns \"fizz\" for n divisible by 3, \"buzz\" for n divisible by 5,
+\"fizzbuzz\" for n divisible by 5 and 3, otherwise just return n."
+  [n]
+  (cond (zero?     n) 0
+        (fizzbuzz? n) "fizzbuzz"
+        (fizz?     n) "fizz"
+        (buzz?     n) "buzz"
+        :else         n))
+
+;; (map fizzbuzz (range 0 100)) ;=>
+
+;; (0 1 2 "fizz" 4 "buzz" "fizz" 7 8 "fizz" "buzz" 11 "fizz" 13 14
+;; "fizzbuzz" 16 17 "fizz" 19 "buzz" "fizz" 22 23 "fizz" "buzz" 26
+;; "fizz" 28 29 "fizzbuzz" 31 32 "fizz" 34 "buzz" "fizz" 37 38 "fizz"
+;; "buzz" 41 "fizz" 43 44 "fizzbuzz" 46 47 "fizz" 49 "buzz" "fizz" 52
+;; 53 "fizz" "buzz" 56 "fizz" 58 59 "fizzbuzz" 61 62 "fizz" 64 "buzz"
+;; "fizz" 67 68 "fizz" "buzz" 71 "fizz" 73 74 "fizzbuzz" 76 77 "fizz"
+;; 79 "buzz" "fizz" 82 83 "fizz" "buzz" 86 "fizz" 88 89 "fizzbuzz" 91
+;; 92 "fizz" 94 "buzz" "fizz" 97 98 "fizz")
